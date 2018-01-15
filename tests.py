60775b800ce18079930d0f53089a67ccb5ed94a8
pollpeterh <pollpeter.h@gmail.com>
2018-01-14 21:06:39 -0600

    Added Google Geocoding

--- tests.py
+++ tests.py
@@ -5,10 +5,12 @@ import Weather
 class WeatherTestCase(unittest.TestCase):
     def test_city_not_found(self):
         with self.assertRaisesRegex(Exception, 'ERROR 404 .+'):
-            Weather.get_temperature("NOT A CITY")
+            Weather.get_temperature_by_city("NOT A CITY")
 
     def test_city_found(self):
-        temperature = Weather.get_temperature('Des Moines')
+        temperature = Weather.get_temperature_by_city('Des Moines')
+        self.assertIsInstance(temperature, int)
+        temperature = Weather.get_temperature_by_city('Des Moines IA')
         self.assertIsInstance(temperature, int)
 
 
