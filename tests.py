1e7f5af7fa29b3c823194b99a894eb1d4cbbe45d
pollpeterh <pollpeter.h@gmail.com>
2018-01-16 16:44:39 -0600

    Changed unit test to use new weather file

--- tests.py
+++ tests.py
@@ -1,16 +1,16 @@
 import unittest
-import Weather
+import weather
 
 
 class WeatherTestCase(unittest.TestCase):
     def test_city_not_found(self):
         with self.assertRaisesRegex(Exception, 'ERROR ZERO_RESULTS'):
-            Weather.get_temperature_by_city("notacity")
+            weather.get_temperature_by_city("notacity")
 
     def test_city_found(self):
-        temperature = Weather.get_temperature_by_city('Des Moines')
+        temperature = weather.get_temperature_by_city('Des Moines')
         self.assertIsInstance(temperature, int)
-        temperature = Weather.get_temperature_by_city('Des Moines IA')
+        temperature = weather.get_temperature_by_city('Des Moines IA')
         self.assertIsInstance(temperature, int)
 
 
